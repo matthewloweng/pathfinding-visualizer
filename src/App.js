518325a6a34e58b878a1003e9f76dd1f87671498
Matthew Ng <matthew.j.ng@gmail.com>
2024-04-05 10:29:32 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <h1 className="header">Pathfinding Visualizer</h1>
+        <button>Visualize Dijkstra's Algorithm</button>
+        <button>Visualize BFS</button>
+        <button>Visualize DFS</button>
+        <button>Reset Grid</button>
+      </header>
       <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
